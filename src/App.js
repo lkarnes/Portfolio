f941919362d035e54cb95d24d377ff889e7607fd
Logan Karnes <logan.karnes98@gmail.com>
2019-10-08 20:16:54 -0700

    before less delete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './App.css';
 import projectsData from './data';
 import About from './components/About.js'
+import './index.css'
 
 import Header from './components/Header.js';
 import Projects from './components/Projects.js';
