37c856b02af09e8b97ef46bea9b540e63be4b7b0
Logan Karnes <logan.karnes98@gmail.com>
2019-12-01 15:58:59 -0800

    added contact me section

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ export default function Header(){
     return (
         <div className='header'>
             <div className='navigation'>
-                <a href='#Projects' className='nav-link'>My Projects</a>
                 <a href='#About' className='nav-link'>About Me</a>
+                <a href='#Projects' className='nav-link'>My Projects</a>
                 <a href='#Resume' className='nav-link'>Resume</a>
             </div>
             <div className='heading'>
@@ -14,7 +14,6 @@ export default function Header(){
                 <p className='short-dis'>I am a Full Stack Web Developer from Lambda School who specailizes in React.js.<br/>**Currently under construction***</p>
                 <a href='#About'><img className='arrow' src={Arrow}/></a>    
             </div>
-            
         </div>
     )
 }
