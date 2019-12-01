d2ab744d473b021e3d12bc43ec76e9f6de3d42f0
Logan Karnes <logan.karnes98@gmail.com>
2019-12-01 13:21:29 -0800

    to push to master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import projectsData from './data';
 import About from './components/About.js'
-import './index.css'
 
 import Header from './components/Header.js';
 import Projects from './components/Projects.js';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './sass/index.scss'
+import './fonts/Montserrat-Black.ttf'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
