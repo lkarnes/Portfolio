375fc30c7d84fe4f3ba1a66de3269e7a973fc82a
Logan Karnes <logan.karnes98@gmail.com>
2020-07-13 19:38:29 -0700

    fixed error

--- src/components/SingleProjectView.js
+++ src/components/SingleProjectView.js
@@ -13,13 +13,14 @@ export default function SingleProjectView(props) {
             }
         })
     },[])
-    console.log(project.skills)
     return (
         <div className='project-view'>
             
             <div className='project-box'>
-            <Link to='/dashboard' className='back'><img src={Back}/></Link>
-                <img src={project.img}/>
+            <Link to='/dashboard' className='back'>
+                <img src={Back} alt='back'/>    
+            </Link>
+                <img src={project.img} alt='project'/>
                 <div className='project-content'>
                     <h3>{project.title}</h3>
                     <p className='date'>{project.date}</p>
